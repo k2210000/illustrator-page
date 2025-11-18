bc9c00ec7c59f3267145347b59ce724a2d8f6c70
Phantom <k22100000@gmail.com>
2025-11-18 17:59:43 +0800

    正式開始設計

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from "astro:content";
+
+const illustrators = defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+        z.object({
+            name: z.string(),                             // 繪師名稱
+            status: z.enum(["commissioned", "wishlist"]), // 已委託 / 想委託
+            avatar: image(),                              // 代表圖片
+            tags: z.array(z.string()).optional(),         // 類型標籤
+            links: z
+                .object({
+                    twitter: z.string().url().optional(),
+                    pixiv: z.string().url().optional(),
+                    website: z.string().url().optional(),
+                })
+                .optional(),
+            imageLink: z.string().url().optional()
+        }),
+});
+
+export const collections = {
+    illustrators,
+};
